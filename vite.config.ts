b07b25504ae960636e891751d0f0e821226ded9f
milanicicibank-ux <milanicicibank@gmail.com>
2026-02-12 17:32:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // This ensures all files (JS, CSS) are loaded relative to the current folder
   base: './', 
   plugins: [react()],
   build: {
